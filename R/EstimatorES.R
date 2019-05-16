5ce9b21dcb94f43eb7f4e9a14e7561cd09b6e13f
Anthony Christidis <anthony.christidis@stat.ubc.ca>
2019-05-16 04:27:21 -0700

    Fix weight for adaptive method.

--- R/EstimatorES.R
+++ R/EstimatorES.R
@@ -87,7 +87,8 @@ EstimatorSE = function(data, ...,
         IFcorPW = SE.xts(data, SE.IF.cor, myfun, myfun.IF, prewhiten=TRUE, ...)
         if(0<=ar1.param & ar1.param<a)
           w = 0 else if(a<=ar1.param & ar1.param<=b)
-            w = (ar1.param - a)/(b - a)
+            w = (ar1.param - a)/(b - a) else
+              w = 1
         res = (1-w)*IFcor + w*IFcorPW
         names(res) = NULL
         } else{
